1c98211e05047f602d5ea0533c5f55c176f7203e
lukec11 <lukec11@users.noreply.github.com>
2020-08-02 16:31:31 +0000

    Prettified Code!

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-	tabWidth: 4,
-	trailingComma: 'es5',
-	arrowParents: 'always',
-	semi: true,
-	singleQuote: true
-}
+    tabWidth: 4,
+    trailingComma: 'es5',
+    arrowParents: 'always',
+    semi: true,
+    singleQuote: true,
+};
